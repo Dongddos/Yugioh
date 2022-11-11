6fa9cad1260f14ed435db612668204ebedb4bb9d
Dongddos <114302841+Dongddos@users.noreply.github.com>
2022-11-12 06:33:03 +0700

    Update yugioh.js

--- yugioh.js
+++ yugioh.js
@@ -16,6 +16,15 @@ module.exports.run = async function({ api, event, args, Users }) => {
             var all = args.slice(1).join(" ");
             const ygo = await axios.get(`https://db.ygoprodeck.com/api/v7/cardinfo.php?${args[0]}=${all}`);
             var name = ygo.data.data[0].data.name;
+            var id = ygo.data.data[0].data.id;
+            var type = ygo.data.data[0].data.type;
+            var des = ygo.data.data[0].data.desc;
+            var atk = ygo.data.data[0].data.atk;
+            var def = ygo.data.data[0].data.def;
+            var level = ygo.data.data[0].data.level;
+            var race = ygo.data.data[0].data.race;
+            var attribute = ygo.data.data[0].data.attribute;
+            var archetype = ygo.data.data[0].data.archetype;
         }
     }
 }
